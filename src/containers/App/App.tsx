8a80a1f913431187b53553b0a50a32ed480272d8
Bobby Mundy <rmundy@fulcrumit.com>
2018-08-08 17:20:32 -0400

    refactoring

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -38,21 +38,23 @@ export default class App extends React.Component<IAppProps, IAppState> {
   };
 
   public render() {
-    const {robots, searchField} = this.state;
+    const { robots, searchField } = this.state;
     const filteredRobots = robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(searchField.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    return (
-      <div className="tc">
-        <h1 className="f1">ROBOFRIENDS</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <CardList robots={filteredRobots} />
-        </Scroll>
-      </div>
-    );
+    if (!robots.length) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">ROBOFRIENDS</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
